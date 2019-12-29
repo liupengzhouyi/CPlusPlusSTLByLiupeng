8ff981e4a9a1e90eb996bf02e7dbd4c7b51268ef
liupeng <liupeng.0@outlook.com>
2019-12-29 23:49:55 +0800

    StringByLiuPeng V 2.0.1

---
+++ Function/AboutStrlen.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 刘鹏 on 2019/12/29.
+//
+
+#include <iostream>
+
+void paly(){
+    std::cout << "hj" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include "StringByLiuPeng/StringByLiuPeng.h"
-
-void paly(char ch[]) {
-    std::cout << ch << std::endl;
-}
+#include "Function/AboutStrlen.h"
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
     StringByLiuPeng * stringByLiuPeng;
     char *ch = "ghj765k";
     stringByLiuPeng = new StringByLiuPeng(ch);
     stringByLiuPeng = new StringByLiuPeng("cfvgbhjnkm");
     std::cout << stringByLiuPeng->longth();
+
     return 0;
 }
